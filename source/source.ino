1a00ddc3264f4d3701b948010d8c0185787484fc
lifaet <lifaethossain@gmail.com>
2023-11-26 16:05:37 +0600

    Update source.ino

--- source/source.ino
+++ source/source.ino
@@ -17,11 +17,13 @@ RST to D9
 #include <SPI.h>
 #include <Servo.h>
 #include <MFRC522.h>
+#include <LiquidCrystal_I2C.h>
 #define buzzerPin 6
 #define sensorPin1 A2
 #define sensorPin2 A3
 #define RST_PIN 9
 #define SS_PIN 10
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 Servo servo;
 int senVal1 = 0;
 int senVal2 = 0;
@@ -38,6 +40,18 @@ void setup() {
   SPI.begin();
   mfrc522.PCD_Init();
   Serial.println("Automatic toll Collection System");
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.print("    Welcome    ");
+  delay(1500);
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("   Automatic   ");
+  lcd.setCursor(0, 1);
+  lcd.print("Toll Collection");
+  delay(1500);
+  lcd.clear();
   delay(1000);
 }
 
@@ -47,11 +61,18 @@ void loop() {
   if (senVal1 == 0) {
     servoDown();
     Serial.println("Vehicle Detected. Swipe you card..");
+    lcd.clear();
+    lcd.setCursor(0, 1);
+    lcd.print("Vehicle Detected");
     delay(1000);
   } else if (senVal2 == 0 && state == 1) {
     servoUp();
     Serial.println("Have a safe Journey *_*");
-    delay(1000);
+    lcd.clear();
+    lcd.setCursor(0, 1);
+    lcd.print("  Safe Journery  ");
+    delay(1500);
+    lcd.clear();
     state = 0;
   } else if (senVal2 == 0 && state == 0) {
     accessDenied();
@@ -87,8 +108,24 @@ void readRfid() {
   }
   content.toUpperCase();
   if (content.substring(1) == "B0 54 95 32") {
+    lcd.clear();
+    lcd.setCursor(0, 1);
+    lcd.print("   Toll Paid   ");
     state = 1;
   } else if (content.substring(1) == "A9 9C 09 BA") {
+    lcd.clear();
+    lcd.setCursor(0, 1);
+    lcd.print("   Toll Paid   ");
+    state = 1;
+  } else if (content.substring(1) == "92 CA BA 1D") {
+    lcd.clear();
+    lcd.setCursor(0, 1);
+    lcd.print("   Toll Paid   ");
+    state = 1;
+  } else if (content.substring(1) == "7C D0 8F 81") {
+    lcd.clear();
+    lcd.setCursor(0, 1);
+    lcd.print("   Toll Paid   ");
     state = 1;
   } else {
     accessDenied();
@@ -99,6 +136,9 @@ void accessDenied() {
   servo.attach(8);
   servo.write(90);
   Serial.println("Access denied");
+  lcd.clear();
+  lcd.setCursor(0, 1);
+  lcd.print(" Access denied ");
   digitalWrite(buzzerPin, HIGH);
   delay(1500);
   digitalWrite(buzzerPin, LOW);
